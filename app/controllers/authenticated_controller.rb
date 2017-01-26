fe5f28d07f12e0bfc176f4c71f7b2f9581803880
Akinyele Akintola-Febrissy <aakin013@uottawa.ca>
2017-01-25 20:53:45 -0500

    New fixes...

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -4,6 +4,13 @@ class AuthenticatedController < ApplicationController
         unless logged_in?
             redirect_to '/login'
         end
+
+        if params['controller'] == 'shows'
+            p "NO CACHE!!!"
+            response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+            response.headers["Pragma"] = "no-cache"
+            response.headers["Expires"] = "Fri, 01 Jan 1900 00:00:00 GMT"
+        end
     }
 
 end
