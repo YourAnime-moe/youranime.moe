b16261dddb9cc316e82dc2fe4d87e8d4037311a8
Akinyele Akintola-Febrissy <aakin013@uottawa.ca>
2017-12-18 23:38:23 -0500

    Added new tests

--- test/controllers/shows_controller_test.rb
+++ test/controllers/shows_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class ShowsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    test "New show should be subbed by default" do
+        show = Show.new
+        show.save
+        assert show.subbed
+    end
+
+    test "New show should not be dubbed by default" do
+        show = Show.new
+        show.save
+        assert_not show.dubbed
+    end
+
+    test "Subbed shows should have subs" do
+        show = Show.new
+        show.subbed = true
+        show.title = "My subbed show"
+        show.save
+
+        assert show.subbed
+
+        (1..10).each do |e|
+            episode = Episode.new
+            episode.show_id = show.id
+            assert episode.has_subs?
+        end
+    end
+
+    test "Dubbed shows should not have subs" do
+        show = Show.new
+        show.dubbed = true
+        show.title = "My dubbed show"
+        show.save
+
+        assert show.dubbed
+
+        (1..10).each do |e|
+            episode = Episode.new
+            episode.show_id = show.id
+            assert_not episode.has_subs?
+        end
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ require 'rails/test_help'
 require 'coveralls'
 Coveralls.wear!
 
+ActiveRecord::Migration.maintain_test_schema!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
