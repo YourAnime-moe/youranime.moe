550b862067d06e89def70f2f972d333dae5c5ae9
Akinyele Akintola-Febrissy <aakin013@uottawa.ca>
2018-02-23 22:22:27 -0500

    Added user to response JSON

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -60,7 +60,7 @@ class ApiController < ApplicationController
         end
  
         if success
-			render json: {token: user.auth_token, message: "Welcome, #{user.get_name}!", success: true}
+			render json: {token: user.auth_token, message: "Welcome, #{user.get_name}!", user: user, success: true}
 		else
 			render json: {message: "Sorry, our server authenticated you but could not log you in.", success: false}
 		end
