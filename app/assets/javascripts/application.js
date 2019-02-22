13999323e895c6c450741e140ac7a00dfcabccb2
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-02-22 15:39:59 -0500

    Login page: OK

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,3 +42,83 @@ function retreiveMessages(container) {
         error: errorFunction
     });
 }
+
+function humanFileSize(bytes, si) {
+    si = si === undefined ? true : false;
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+}
+
+var ajaxQueue = $({});
+var currentRequest = null;
+$.ajaxQueue = function( ajaxOpts ) {
+    // Hold the original complete function.
+    var oldComplete = ajaxOpts.complete;
+    // Queue our ajax request.
+    ajaxQueue.queue(function( next ) {
+        // Create a complete callback to fire the next event in the queue.
+        ajaxOpts.complete = function() {
+            // Fire the original complete if it was there.
+            if ( oldComplete ) {
+                oldComplete.apply( this, arguments );
+            }
+            // Run the next query in the queue.
+            next();
+        };
+        // Run the query.
+        currentRequest = $.ajax( ajaxOpts );
+    });
+};
+
+// Abort method
+function abortAjaxQueue() {
+    ajaxQueue.clearQueue();
+    if (currentRequest) {
+        currentRequest.abort();
+    }
+}
+
+$(document).ready(function() {
+      //$(".button-collapse").sidenav();
+      //$('.modal').modal();
+      //$(".dropdown-trigger").dropdown();
+      //$('select').formSelect();
+      //$('.datepicker').datepicker();
+
+      var current_lang = navigator.language || navigator.userLanguage || 'en';
+      if (current_lang == 'ja') {
+        current_lang = 'jp';
+      }
+      switchTo(current_lang, false);
+
+      [].forEach.call(document.querySelectorAll('[locale-switcher]'), function(elem) {
+        elem.onclick = function(e) {
+          switchTo(elem.getAttribute('locale-switcher'), true);
+        }
+      });
+  });
+
+  function switchTo(locale, force) {
+    $.ajax({
+      url: '/set/current/locale.json',
+      method: 'put',
+      data: {locale: locale, set_at_first: force},
+      success: function(res) {
+        console.log(res);
+        if (res.reload) {
+          location.href = location.href;
+        }
+      }
+    })
+  }
