aa3a6be1c96d1f48ed4493d78cd5cd0ec35f84bc
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-04-15 23:53:54 -0400

    play pause works

--- app/assets/javascripts/episodes.js
+++ app/assets/javascripts/episodes.js
@@ -2,14 +2,22 @@ $(document).on('turbolinks:load', function() {
   // Global variables
   var goingToShow = false;
   var goingToEpisode = false;
-  
-  var video = null;
+
+  var $video = null;
+  var $playing_btn = null;
+  var $paused_btn = null;
 
   if ($('.video-body').size() > 0) {
+    $playing_btn = $('.playing');
+    $paused_btn = $('.paused');
+
     $('[go-to-show]').on('click', goToShow);
     $('[go-to-episode]').on('click', goToEpisode);
+    $('[hf-action]').on('click', execHfAction);
 
-    video = $('#video-obj')
+    $video = $('#video-obj');
+    $video.on('play', onPlay);
+    $video.on('pause', onPause);
 
     function goToShow(e) {
       if (goingToShow) {
@@ -34,11 +42,47 @@ $(document).on('turbolinks:load', function() {
       });
     }
 
-    function pause(callback) {
+    function execHfAction(e) {
+      if (this.getAttribute('hf-action') === 'toggle-play-pause') {
+        togglePlayPause();
+      }
+    }
 
+    function togglePlayPause(callback) {
+      if (isPlaying()) {
+        pause(callback);
+      } else {
+        play(callback);
+      }
+    }
+
+    function play(callback) {
+      $video.get(0).play().then(callback);
+    }
+
+    function onPlay(e) {
+      $playing_btn.addClass('active');
+      $paused_btn.removeClass('active');
+    }
+
+    function onPause(e) {
+      $playing_btn.removeClass('active');
+      $paused_btn.addClass('active');
+    }
+
+    function pause(callback) {
+      $video.get(0).pause();
       if (typeof(callback) === 'function') {
         callback();
       }
+    }
+
+    function isPlaying() {
+      return !isPaused();
+    }
+
+    function isPaused() {
+      return $video.get(0).paused;
     }
   }
 });
