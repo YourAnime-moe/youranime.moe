02575406c7d1f2f8c2d383f8d5ba660c91fd93a7
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-04-16 12:12:07 -0400

    player basically complete

--- app/assets/javascripts/episodes.js
+++ app/assets/javascripts/episodes.js
@@ -2,22 +2,31 @@ $(document).on('turbolinks:load', function() {
   // Global variables
   var goingToShow = false;
   var goingToEpisode = false;
+  var fullscreenOn = false;
+  var videoLoaded = false;
   var inactivityTime = 5000;
   var timeoutId;
 
   var $video = null;
+  var $progress = null;
+  var $progressBar = null;
   var $playing_btn = null;
   var $paused_btn = null;
 
   if ($('.video-body').size() > 0) {
     $playing_btn = $('.playing');
     $paused_btn = $('.paused');
+    $progress = $('.progress-filled');
+    $progressBar = $('.progress-bar');
+    $video = $('#video-obj');
 
     $('[go-to-show]').on('click', goToShow);
     $('[go-to-episode]').on('click', goToEpisode);
     $('[hf-action]').on('click', execHfAction);
 
-    $video = $('#video-obj');
+    $('.video-clickable-zone').on('click', togglePlayPause);
+    $('.video-clickable-zone').on('dblclick', toggleFullscreen);
+    $progressBar.on('click', seek);
     $video.on('play', onPlay);
     $video.on('timeupdate', onProgress);
     $video.on('pause', onPause);
@@ -28,6 +37,7 @@ $(document).on('turbolinks:load', function() {
     document.addEventListener("mousedown", cancelFadeOutInControls, false);
     document.addEventListener("keypress", cancelFadeOutInControls, false);
     document.addEventListener("touchmove", cancelFadeOutInControls, false);
+    document.addEventListener('keypress', spaceBarTogglePlay);
 
     setFadeOutInControls();
 
@@ -60,6 +70,12 @@ $(document).on('turbolinks:load', function() {
       }
     }
 
+    function spaceBarTogglePlay(e) {
+      if (e.keyCode == 32) {
+        togglePlayPause();
+      }
+    }
+
     function togglePlayPause(callback) {
       if (isPlaying()) {
         pause(callback);
@@ -69,9 +85,10 @@ $(document).on('turbolinks:load', function() {
     }
 
     function setVideoDuration() {
-      var hours = parseInt($video.get(0).duration / 60 / 60, 10);
-      var minutes = parseInt($video.get(0).duration / 60, 10);
-		  var seconds = parseInt($video.get(0).duration % 60);
+      var duration = $video.get(0).duration;
+      var hours = parseInt(duration / 60 / 60, 10);
+      var minutes = parseInt(duration / 60, 10);
+		  var seconds = parseInt(duration % 60);
 
       min_sec = stringifyInts(minutes) + ':' + stringifyInts(seconds);
       if (hours > 0) {
@@ -81,6 +98,7 @@ $(document).on('turbolinks:load', function() {
         $('time').children('.hours').hide();
       }
       $('time').children('.duration').text(min_sec);
+      videoLoaded = true;
     }
 
     function stringifyInts(value) {
@@ -97,15 +115,42 @@ $(document).on('turbolinks:load', function() {
     }
 
     function onProgress(e) {
-      var start = this.currentTime;
-      var total = this.duration;
-      var loadPercentage = (start/total)*100;
+      var currentTime = this.currentTime;
+      var duration = this.duration;
+      var loadPercentage = (currentTime / duration) * 100;
+      $progress.css({width: `${loadPercentage}%`})
 
+      var timeleft = duration - currentTime;
+      var hours = parseInt(timeleft / 60 / 60, 10);
+      var minutes = parseInt(timeleft / 60, 10);
+		  var seconds = parseInt(timeleft % 60);
+
+      min_sec = stringifyInts(minutes) + ':' + stringifyInts(seconds);
+      if (hours > 0) {
+        hours = stringifyInts(hours) + ':'
+        $('time').children('.hours').show().text(hours);
+      } else {
+        $('time').children('.hours').hide();
+      }
+      $('time').children('.duration').text(min_sec);
     }
 
     function onPlay(e) {
       $playing_btn.addClass('active');
       $paused_btn.removeClass('active');
+    }
+
+    function seek(e) {
+      if (!videoLoaded) {
+        return;
+      }
+      pause(function() {
+        var video = $video.get(0);
+        var progress = $progressBar.get(0);
+        video.currentTime = (e.offsetX / progress.offsetWidth) * video.duration;
+        play();
+      });
+
     }
 
     function onPause(e) {
@@ -155,6 +200,42 @@ $(document).on('turbolinks:load', function() {
     function showControls() {
       $('.video-body').removeClass('muchuu');
       $('.controls-container').fadeIn();
+    }
+
+    function toggleFullscreen() {
+      if (fullscreenOn) {
+        exitFullscreen();
+      } else {
+        enterFullscreen($('.video-body').get(0));
+      }
+    }
+
+    function enterFullscreen(video) {
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+      } else if (video.mozRequestFullScreen) {
+        video.mozRequestFullScreen();
+      } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen();
+      } else if (video.msRequestFullscreen) {
+        video.msRequestFullscreen();
+      }
+    }
+
+    function exitFullscreen() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
+
+    function onFullscreenChange(e) {
+      fullscreenOn = document.fullscreenElement === e.target;
     }
   }
 });
