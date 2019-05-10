28b2272c7d1e852f88cf0579d13a66b194368706
Akinyele Cafe-Febrissy <akinyele.cafefebrissy@shopify.com>
2019-05-10 09:27:29 -0400

    Change container classes and config parameteres

--- config/application.rb
+++ config/application.rb
@@ -20,11 +20,3 @@ module HaveFun
     config.action_controller.default_protect_from_forgery = true
   end
 end
-
-
-Config.setup do |config|
-  config.domain = ENV['MAIN_HOST'] || ''
-  config.videojs = '5.16.0'
-  config.bulma_version = '0.7.4'
-  config.api_version = 1
-end
