e8a3d14b4a59e173b98027e2288eeb6d5fc418d1
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-05-23 16:53:51 -0400

    Replace minitest in favour of rspec

--- test/models/show_test.rb
+++
@@ -1,5 +0,0 @@
-require 'models/tanoshimu_base_test'
-
-class ShowTest < TanoshimuBaseTest
-
-end
--- test/models/subtitle_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SubtitleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
