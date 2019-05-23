9760c1fd75aec3a86f6603dafee035e9829f5c9c
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-05-23 16:25:48 -0400

    Clean up `episode.rb`

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,7 +1,4 @@
 class Episode < ApplicationRecord
-  include Navigatable
-
-  self.table_name = 'episodes'
 
   has_one_attached :video
   has_one_attached :thumbnail
@@ -19,9 +16,7 @@ class Episode < ApplicationRecord
   end
 
   def is_published?
-    return false if self.published.nil? && !self.show.nil?
-    return self.published if self.show.nil?
-    self.show.is_published? && self.published
+    !!(published && show&.is_published?)
   end
 
   def unrestricted?
@@ -32,59 +27,13 @@ class Episode < ApplicationRecord
     !unrestricted?
   end
 
-  def get_path_extension
-    return nil if self.path.nil?
-    parts = self.path.split "."
-
-    # Only accept the last part, don't accept multiple extensions for videos
-    parts[parts.size-1]
-  end
-
-  def has_previous?
-    !self.previous.nil?
-  end
-
-  def has_next?
-    !self.next.nil?
-  end
-
-  def has_valid_subtitles?
+  def has_subtitles?
     subtitles.select{|subtitle| subtitle.src.attached?}.size > 0
   end
 
   def next
     return nil unless show
     show.episodes.where(['episode_number > ?', episode_number]).first
-  end
-
-  def get_path(token=nil, as_is: false)
-    return self.path if self.path.nil? or self.path.start_with? "http"
-    res = Config.path self.path, as_is: as_is
-    return res if token.nil?
-    res + '?token=' + token
-  end
-
-  def get_new_path
-    return self.path if self.path.nil? or self.path.start_with? "http"
-    video_path = self.path
-    parts = video_path.split '/'
-    return nil if parts.size <= 2
-
-    # Get the general information
-    filename = parts[parts.size-1]
-    show_name = parts[parts.size-2]
-
-    # Get the episiode number and extension
-    filename_parts = filename.split '.'
-    ext = filename_parts[filename_parts.size-1]
-    filename_name = filename_parts[0].split 'ep'
-    ep_num = filename_name[1] || self.episode_number
-
-    Config.path "videos?show=#{show_name}&episode=#{ep_num}&format=#{ext}&video=true"
-  end
-
-  def has_thumbnail?
-    get_thumbnail(raise_exception: false).attached?
   end
 
   def get_thumbnail(raise_exception: false)
@@ -108,7 +57,7 @@ class Episode < ApplicationRecord
   end
 
   def has_video?
-    get_video(raise_exception: false).attached?
+    !!(video&.attached?)
   end
 
   def get_video(raise_exception: false)
@@ -131,89 +80,12 @@ class Episode < ApplicationRecord
     get_video.service_url(expires_in: expires_in)
   end
 
-  def get_image_path(token=nil, ext: 'jpg', as_is: false)
-    video_path = self.get_path(as_is: as_is)
-    return nil if video_path.nil?
-    parts = video_path.split('/')
-    filename = parts[parts.size-1]
-    return nil if filename.nil?
-    fs = filename.split '.'
-    fs[1] = ext.to_s
-    filename = fs.join '.'
-    parts[parts.size-1] = filename
-    path = parts.join '/'
-    return path if token.nil?
-    path + "?token=" + token
-  end
-
-  def get_new_image_path(ext: 'jpg')
-    return self.path if self.path.nil? or self.path.start_with? "http"
-    video_path = self.path
-    parts = video_path.split '/'
-    return nil if parts.size <= 2
-
-    # Get the general information
-    filename = parts[parts.size-1]
-    show_name = parts[parts.size-2]
-
-    # Get the episiode number and extension
-    filename_parts = filename.split '.'
-    filename_name = filename_parts[0].split 'ep'
-    ep_num = filename_name[1] || self.episode_number
-
-    Config.path "videos?show=#{show_name}&episode=#{ep_num}&format=#{ext}"
-  end
-
-  def get_subtitle_path(ext: 'vtt')
-    return nil if !self.show.nil? && self.show.dubbed
-    get_image_path ext: ext
-  end
-
-  def get_new_subtitle_path(ext: 'vtt')
-    path = get_new_image_path ext: ext
-    path + "&subtitles=true"
-  end
-
-  def has_subs?
-    self.show.subbed ? true : false
-  end
-
-  def was_watched_by?(user)
-    return false if user.episodes_watched.nil? or user.episodes_watched.empty?
-    !user.episodes_watched.select { |id| id == self.id }.empty?
-  end
-
-  def has_watched_mark?
-    !get_watched_mark.nil?
-  end
-
-  def get_watched_mark
-    80
-  end
-
   def set_progress(current_user, progress)
     video.attached? && progress(current_user).update(progress: progress)
   end
 
   def progress(current_user)
     UserWatchProgress.find_or_create_by(episode_id: id, user_id: current_user.id)
-  end
-
-  def progress_info user, value_only: false
-    info = {
-      user: user.id,
-      episode: self.id,
-      progress_info: nil
-    }
-    default_value = self.was_watched_by?(user) ? 97 : 0
-    return (value_only ? default_value : info) if user.episode_progress_list.nil? || user.episode_progress_list.empty?
-    info[:progress_info] = user.episode_progress_list.select{|chunk| chunk[:id] == self.id}[0]
-    info[:found] = !info[:progress_info].nil?
-    if value_only
-      return 0 if info[:progress_info].nil?
-      return info[:progress_info][:progress]
-    end
-    info
   end
 
   def as_json(options={})
