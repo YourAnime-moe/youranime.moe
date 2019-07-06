4cbbf7d56415ddf243cd7e9565c9060c1930a1c4
Akinyele Cafe-Febrissy <akinyele.cafefebrissy@shopify.com>
2019-07-06 12:50:13 -0400

    Show shows by 4 on shows index

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,24 +3,20 @@
 class ShowsController < AuthenticatedController
   include ShowsHelper
 
+  before_action :ensure_show_is_available!, only: [:show]
+
   def index
     set_title(before: t('anime.shows.view-all'))
     @shows = published_shows
     @shows_count = @shows.count
     @additional_main_class = 'no-margin no-padding' if @shows.blank?
-    @shows_parts = @shows.each_slice(3).to_a
+    @shows_parts = @shows.each_slice(4).to_a
   end
 
   def show
-    @show = Show.find_by(id: params[:id].to_i)
-    if unavailable?(@show)
-      flash[:warning] = 'This show is not available yet. Please try again later.'
-      redirect_to shows_path
-    else
-      set_title(before: @show.title)
-      @episodes_parts = @show.episodes.each_slice(3).to_a
-      @additional_main_class = 'no-margin no-padding'
-    end
+    set_title(before: @show.title)
+    @episodes_parts = @show.episodes.each_slice(3).to_a
+    @additional_main_class = 'no-margin no-padding'
   end
 
   private
@@ -28,7 +24,15 @@ class ShowsController < AuthenticatedController
   def published_shows
     return Show.search(params[:query]) if params[:query].present?
 
-    Show.published
+    Show.published.includes(:episodes)
+  end
+
+  def ensure_show_is_available!
+    @show = Show.find_by(id: params[:id].to_i)
+    return unless unavailable?(@show)
+
+    flash[:info] = 'This show is not available yet. Please try again later.'
+    redirect_to shows_path
   end
 
   def unavailable?(show)
