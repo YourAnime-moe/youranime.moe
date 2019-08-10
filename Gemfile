14464dc90380a1376c4c35446f1ebda921cddf57
Akinyele Cafe-Febrissy <akinyele.cafefebrissy@shopify.com>
2019-08-10 17:40:29 -0400

    Add specs for Issue

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'sass-rails'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+# As a state machine (AASM)
+gem 'aasm'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,20 +1,46 @@
 class Issue < ApplicationRecord
+  include AASM
   include ConnectsToShowsConcern
-  
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :status, presence: true
 
-  belongs_to :user, inverse_of: :issues
-  validate :page_url_format
+  OPEN = :open
+  PENDING = :pending
+  IN_PROGRESS = :in_progress
+  RESOLVED = :resolved
+  CLOSED = :closed
+  ARCHIVED = :archived
 
+  STATUSES = [OPEN, PENDING, IN_PROGRESS, RESOLVED, CLOSED, ARCHIVED].freeze
   PAGE_URL_FORMAT = /\A\/[\/\w-]+\z/
 
-  private
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :status, inclusion: { in: STATUSES }
 
-  def page_url_format
-    unless PAGE_URL_FORMAT === page_url
-      errors.add(:page_url, 'is not valid. Did you add query parameters?')
+  belongs_to :user, inverse_of: :issues
+  validates_format_for :page_url, with: PAGE_URL_FORMAT, if: :page_url?
+
+  aasm column: :status do
+    state OPEN, initial: true
+    (STATUSES - [OPEN]).each { |status| state(status) }
+
+    event :close do
+      transitions from: [OPEN, IN_PROGRESS, PENDING], to: CLOSED
+    end
+
+    event :as_pending do
+      transitions from: OPEN, to: PENDING
+    end
+
+    event :as_in_progress do
+      transitions from: PENDING, to: IN_PROGRESS
+    end
+
+    event :archive do
+      transitions from: [PENDING, CLOSED, RESOLVED], to: ARCHIVED
+    end
+
+    event :resolve do
+      transitions from: IN_PROGRESS, to: RESOLVED
     end
   end
 end
