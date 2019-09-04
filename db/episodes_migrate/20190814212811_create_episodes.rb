d5f43a4b89353b06fb783e2714850914d9a2e281
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-09-04 08:47:28 -0400

    Connects to episodes

---
+++ db/episodes_migrate/20190814212811_create_episodes.rb
@@ -0,0 +1,20 @@
+class CreateEpisodes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :episodes do |t|
+      t.integer :season_id, null: false
+      t.integer :number, null: false
+      t.string :title, null: false
+      t.float :duration
+      t.integer :views, null: false, default: 0
+      t.string :thumbnail_url
+      t.string :caption_url
+
+      t.timestamps
+
+      t.index [:season_id, :number], unique: true
+      t.index :number
+      t.index :thumbnail_url
+      t.index :caption_url
+    end
+  end
+end
