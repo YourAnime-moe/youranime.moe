92622722f7ef2bd05ffbf1c45f98cc37d0f3ba90
Akinyele Cafe-Febrissy <me@akinyele.ca>
2019-12-26 16:24:06 -0500

    Publish version 1.1.2

--- tanoshimu_utils/lib/tanoshimu_utils.rb
+++ tanoshimu_utils/lib/tanoshimu_utils.rb
@@ -1,7 +1,7 @@
 require 'active_support'
 
 require 'tanoshimu_utils/version'
-require 'tanoshimu_utils/concerns'
+require_relative 'tanoshimu_utils/concerns'
 
 module TanoshimuUtils
   class Error < StandardError; end
