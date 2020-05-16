256a592cdd5d8fe4612e07a9890a9b677e469c0c
[Ubuntu Server] A. Cafe-Febrissy <me@akinyele.ca>
2020-05-16 13:12:13 +0000

    Order season episodes by title

--- app/models/shows/season.rb
+++ app/models/shows/season.rb
@@ -1,7 +1,7 @@
 module Shows
   class Season < ApplicationRecord
     belongs_to :show
-    has_many :episodes, -> { published }
+    has_many :episodes, -> { published.order(:title) }
     has_many :all_episodes, -> { all }, class_name: "Episode"
   end
 end
