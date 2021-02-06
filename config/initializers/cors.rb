5e43b15f59c25b56a9279fddfa74dae587392f7c
Akinyele Cafe-Febrissy <me@akinyele.ca>
2021-02-06 14:16:55 -0500

    Allow more than one origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
-  allowed_origins = Rails.application.config.x.cors_allowed_origins
+  allowed_origins = Rails.application.config.x.cors_allowed_origins.split(',').map(&:strip)
   puts "Allowing CORS origins: #{allowed_origins}"
 
   allow do
-    origins(allowed_origins)
+    origins(*allowed_origins)
 
     resource '*',
       header: 'Content-Type, Authorization',
