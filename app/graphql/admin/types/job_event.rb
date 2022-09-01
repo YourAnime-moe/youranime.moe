4056a86b2236f5c32ebce29437b7467586aaf907
Akinyele Cafe-Febrissy <me@akinyele.ca>
2022-09-01 11:20:56 -0400

    Add relative time for jobs

--- app/graphql/admin/types/job_event.rb
+++ app/graphql/admin/types/job_event.rb
@@ -2,6 +2,8 @@
 module Admin
   module Types
     class JobEvent < ::Types::BaseObject
+      include ActionView::Helpers::DateHelper
+
       connection_type_class ::Types::Custom::BaseConnection
 
       field :id, ID, null: false
@@ -9,12 +11,22 @@ module Admin
       field :job_name, String, null: false
       field :started_at, Integer, null: false
       field :ended_at, Integer, null: true
+      field :started_ago, String, null: false
+      field :ended_ago, String, null: true
       field :job_id, Integer, null: true
       field :model_id, Integer, null: true
       field :used_by_model, String, null: true
       field :failed_reason_key, String, null: true
       field :failed_reason_text, String, null: true
       field :user, Admin::Types::User, null: true
+
+      def started_ago
+        time_ago_in_words(@object.started_at)
+      end
+
+      def ended_ago
+        time_ago_in_words(@object.ended_at) if @object.ended_at
+      end
     end
   end
 end
